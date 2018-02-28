b1ae21c2710962a27903fe9bea35e89915bf28d6
Mustafa Caglar <mc934@cam.ac.uk>
2018-02-28 15:40:15 +0000

    Updated

--- oud_todo.m
+++ oud_todo.m
@@ -1,25 +1,41 @@
 %% PL drop vs Conc for a constant voltage
-% M
-% 0           300118: MQ_KnO3_Cd
-% 0.001       310118: 0_001M_KnO3_Cd2
-% 0.01        300118: 0_01M_KnO3_Cd
-% 0.1         300118: 0_1M_KnO3_Cd
-% 0.25        300118: 0_25M_KnO3_Cd2 0_25M_KnO3_Cd3
-% 0.5         300118: 0_5M_KnO3_Cd2
-% 1           300118: 1M_KnO3_Cd
-
+    % M
+    % 0           300118: MQ_KnO3_Cd
+    % 0.001       310118: 0_001M_KnO3_Cd2
+    % 0.01        300118: 0_01M_KnO3_Cd
+    % 0.1         300118: 0_1M_KnO3_Cd
+    % 0.25        300118: 0_25M_KnO3_Cd2 0_25M_KnO3_Cd3
+    % 0.5         300118: 0_5M_KnO3_Cd2
+    % 1           300118: 1M_KnO3_Cd
 %% Different voltage level, constant conc (PL and V with time)
-%    310118: 1M_KnO3_Cd
-%    310118: 1M_KnO3_InP
-
+    %    310118: 1M_KnO3_Cd
+    %    310118: 1M_KnO3_InP
 %% PL Frequency response
-%    071217: *run14, *run22, *run23_dye
-%
-%
-
+    %    071217: *run1                                                                4, *run22, *run23_dye
 %% RC
-
 %% Current, Voltage, PL plot to show minimal current
-
-
 %% Analysis of 151117, 101117
+%% Negative controls: Water, EL
+%% Positive controls: Variation with concentration? Nano-diamonds?
+%% Camera and laser refresh rate
+%% RC Times etc.
+    %   R1: 35kO +/- 227 O
+    %   C1: 210nF +/- 1 n
+    %   RC -> 0.00735s  -> fc = 21.2 Hz -> rise time 20 to 80% = 0.01029s,  
+    %   rise time 10 to 90% = 0.016s
+%% Number of dots per area   and also the field. So we need electrode area and oncentration of dye/dots 
+%% Legitimate to fit and rmeove bleaching 
+%% Find the frequeny limit -> Perhaps 10Hz? Signal processing to decide through noise. 
+%% Time it takes to form debye layer
+%% Put forward a standard platform -> are we device limited or can we apply this cells? 
+%% Include platelets
+%% Device characteristics and response in the SI perhaps.
+%% Find the fundamental limit and problem in the devices -> where to take the dyes and platform?
+%% Future:
+    %   Poly-electrolyte
+    %   Single dot
+            % with a huge shell (?)
+    %   2D materials
+    
+
+    
